ca6e2330276416b03b25ff418d83fd3bced08ede
yousef <yousef@example.com>
2026-04-10 10:30:00 +0200

    chore: initialize Maven project with Java 17

---
+++ src/main/java/com/cryptotrading/Main.java
@@ -0,0 +1,13 @@
+package com.cryptotrading;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.cryptotrading.models.User;
+import com.cryptotrading.models.Wallet;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
